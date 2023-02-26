e5d512ac1120676995a84bb399676a9039f7e03b
EronAdemi <111318711+EronAdemi@users.noreply.github.com>
2023-02-26 19:25:43 +0100

    Changes to script imports on html

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        index: resolve(__dirname, "index.html"),
         details: resolve(__dirname, "details.html"),
         contact: resolve(__dirname, "contact.html"),
       },
